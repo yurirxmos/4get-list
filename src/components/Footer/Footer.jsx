aea9dbaeb95ed856171d08fd1c46951e3d3689ed
Yuri Ramos <yuriramos2406@gmail.com>
2024-02-23 12:46:01 -0300

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            developed with <b>s2</b> <br />
+            by <a href="https://github.com/yurirxmos" target="_blank">@yurirxmos</a>
+        </div>
+    );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import List from "../../components/List/List";
+import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
   return (
     <div className="main">
       <Header />
       <List />
+      <Footer />
     </div>
   );
 }
